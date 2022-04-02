6f2915ad2b002f665dc3229f38b351093f24bfe1
Alexander Zhivkov <sashko.zhivkov@gmail.com>
2022-04-02 14:47:48 +0300

    first startup

--- src/main/java/bg/tuvarna/sit/usp_cars/common/Constants.java
+++ src/main/java/bg/tuvarna/sit/usp_cars/common/Constants.java
@@ -3,11 +3,11 @@ package bg.tuvarna.sit.usp_cars.common;
 public class Constants {
     public static class View {
 
-        public static final String HELLO_VIEW = "/bg/tu_varna/sit/usp_cars/presentation.views/hello-view.fxml";
+        public static final String HELLO_VIEW = "/bg/tuvarna/sit/usp_cars/presentation.views/hello-view.fxml";
     }
 
     public static class Configurations{
-        public static final String LOG4J_PROPERTIES = "/bg/tu_varna/sit/usp_cars/configuration/log4j.properties";
+        public static final String LOG4J_PROPERTIES = "/bg/tuvarna/sit/usp_cars/configuration/log4j.properties";
     }
 
     public static class Values{
